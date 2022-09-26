a529667f9fbd5219b4e0956e50a2d36cc9523adb
chaitunice <52159476+chaitunice@users.noreply.github.com>
2022-09-26 18:44:02 +0530

    Delete Django/myproject directory

--- Django/myproject/movies/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-
-# Create your views here.
-
-def moviesindex(request):
-    template = loader.get_template('moviesindex.html')
-    return HttpResponse(template.render({}, request))
--- Django/myproject/persons/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Persons(models.Model):
-    fname = models.CharField(max_length=50)
-    lname = models.CharField(max_length=50)
